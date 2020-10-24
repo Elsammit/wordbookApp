78b77fac7dc4da2c3d417348abdfeebeaded8cf8
Elsammit <hhashimo25@gmail.com>
2020-10-24 21:04:30 +0900

    commit Release Rom

---
+++ precache-manifest.e362ae77ade66dc7d2b3b7263b9b2d32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3b1c929d6e078597aebf0c5f8038e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a72d8ddf29c48812d99",
+    "url": "./static/css/main.4386efec.chunk.css"
+  },
+  {
+    "revision": "f2d05c3fec3b99750340",
+    "url": "./static/js/2.08fd704a.chunk.js"
+  },
+  {
+    "revision": "911d17001459141adccf4222c34fea73",
+    "url": "./static/js/2.08fd704a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a72d8ddf29c48812d99",
+    "url": "./static/js/main.13e411ea.chunk.js"
+  },
+  {
+    "revision": "41beda85a9b13053d274",
+    "url": "./static/js/runtime-main.98d931d0.js"
+  },
+  {
+    "revision": "4cdd3cb69ff926d25ffeffa9c54f84c5",
+    "url": "./static/media/BlankCard.4cdd3cb6.png"
+  }
+]);
